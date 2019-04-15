ff188df8583da0319fcb525af9e4421fdb49c874
Patrick Diewald <diewald.patrick@gmail.com>
2019-04-15 09:39:46 +0200

    Added testCA.py script

---
+++ ros_ws/src/crazyswarm/scripts/testCA.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Testing collision avoidance
+#############################
+
+import numpy as np
+
+from pycrazyswarm import *
+
+if __name__ == "__main__":
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs
+
+    cf1 = allcfs.crazyfliesById[1]
+    cf2 = allcfs.crazyfliesById[2]
+
+    #cf1.setGroupMask(0b00000001)
+    #cf2.setGroupMask(0b00000010)
+
+    allcfs.takeoff(targetHeight=0.7, duration=2.0)
+    timeHelper.sleep(2.5)
+
+    z = 1.5
+    TRIALS = 5
+    TIMESCALE = 1.0
+    for i in range(TRIALS):
+
+        pos1 = np.array(cf1.initialPosition) + np.array([1, -3.5, 0.0])
+        cf1.goTo(pos1, 0, 5.0*TIMESCALE)
+        pos2 = np.array(cf2.initialPosition) + np.array([1, -3.5, 0.2])
+        cf2.goTo(pos2, 0, 5.0*TIMESCALE)
+        timeHelper.sleep(5.0*TIMESCALE+0.5)
+
+        pos1 = np.array(cf1.initialPosition) + np.array([1, -3.5, 0.0])
+        cf1.goTo(pos1, 0, 5.0*TIMESCALE)
+        pos2 = np.array(cf2.initialPosition) + np.array([1, -3.5, 0.2])
+        cf2.goTo(pos2, 0, 5.0*TIMESCALE)
+        timeHelper.sleep(5.0*TIMESCALE+0.5)
+
+
+    for cf in allcfs.crazyflies:
+        pos = np.array(cf.initialPosition) + np.array([0, 0, 0.3])
+        cf.goTo(pos, 0, 5.0)
+    timeHelper.sleep(5.5)
+
+    allcfs.land(targetHeight=0.40, duration=2.0)
+    timeHelper.sleep(3.0)
