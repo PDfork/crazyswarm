c0523f1fa846c12792a5cd03e6fe177a1053dadc
PDfork <diewald.patrick@gmail.com>
2019-07-05 16:53:29 +0200

    Changed a bit at collision avoidance

--- ros_ws/src/crazyswarm/scripts/testCA.py
+++ ros_ws/src/crazyswarm/scripts/testCA.py
@@ -22,50 +22,66 @@ if __name__ == "__main__":
     cf2.setParam('sitAw/CAActive', 1)
     cf1.setParam('sitAw/SearchRadius', 3.0)
     cf2.setParam('sitAw/SearchRadius', 3.0)
-    cf1.setParam('sitAw/SeparationRadius', 1.5)
-    cf2.setParam('sitAw/SeparationRadius', 1.5)
+    cf1.setParam('sitAw/SeparationRadius', 3.0)
+    cf2.setParam('sitAw/SeparationRadius', 3.0)
     cf1.setParam('sitAw/TargetRadius', 0.1)
     cf2.setParam('sitAw/TargetRadius', 0.1)
-    cf1.setParam('sitAw/RepGain', 1.0)
-    cf2.setParam('sitAw/RepGain', 1.0)
+    cf1.setParam('sitAw/RepGain', 15.5)
+    cf2.setParam('sitAw/RepGain', 15.5)
+    cf1.setParam('sitAw/MaxSpeed', 0.3)
+    cf2.setParam('sitAw/MaxSpeed', 0.3)
     cf1.setParam('sitAw/Anisotropy', 0.5)
     cf2.setParam('sitAw/Anisotropy', 0.5)
 
-    timeHelper.sleep(10.0)
+    timeHelper.sleep(3.0)
 
     #cf1.setGroupMask(0b00000001)
     #cf2.setGroupMask(0b00000010)
 
-    z = 0.3
-    x_offset = 0.5
+    z = 0.5
+    x_offset = 0.3
     y_offset = 1.0
-    TRIALS = 3
-    TIMESCALE = 1.0
+    TRIALS = 2
+    TIMESCALE = 2.0
+
 
     allcfs.takeoff(targetHeight=z, duration=2.0)
+    for cf in allcfs.crazyflies:
+        pos = np.array(cf.initialPosition) + np.array([0, 0, z])
+        cf.goTo(pos, 0, 2.0)
     timeHelper.sleep(2.5)
 
     for i in range(TRIALS):
 
         pos1 = np.array(cf1.initialPosition) + np.array([x_offset, -y_offset, z])
-        cf1.goTo(pos1, 0, 5.0*TIMESCALE)
+        cf1.goTo(pos1, 0, 0.01*TIMESCALE)
         pos2 = np.array(cf2.initialPosition) + np.array([-x_offset, y_offset, z])
-        cf2.goTo(pos2, 0, 5.0*TIMESCALE)
+        cf2.goTo(pos2, 0, 0.01*TIMESCALE)
         timeHelper.sleep(5.0*TIMESCALE+0.5)
 
         pos1 = np.array(cf1.initialPosition) + np.array([x_offset, y_offset, z])
-        cf1.goTo(pos1, 0, 5.0*TIMESCALE)
+        cf1.goTo(pos1, 0, 0.01*TIMESCALE)
         pos2 = np.array(cf2.initialPosition) + np.array([-x_offset, -y_offset, z])
-        cf2.goTo(pos2, 0, 5.0*TIMESCALE)
+        cf2.goTo(pos2, 0, 0.01*TIMESCALE)
         timeHelper.sleep(5.0*TIMESCALE+0.5)
 
     for cf in allcfs.crazyflies:
         pos = np.array(cf.initialPosition) + np.array([0, 0, 0.1])
-        cf.goTo(pos, 0, 5.0)
-    timeHelper.sleep(5.5)
+        cf.goTo(pos, 0, 2.0)
+    timeHelper.sleep(2.5)
 
-    cf1.setParam('sitAw/CAActive', 0)
-    cf2.setParam('sitAw/CAActive', 0)
+    cf1.setParam('sitAw/Break', 1)
+    cf2.setParam('sitAw/Break', 1)
+
+    timeHelper.sleep(3.0)
+
+    cf1.setParam('sitAw/Break', 0)
+    cf2.setParam('sitAw/Break', 0)
+
+    for cf in allcfs.crazyflies:
+        pos = np.array(cf.initialPosition) + np.array([0, 0, 0.1])
+        cf.goTo(pos, 0, 5.0*TIMESCALE)
+    timeHelper.sleep(5.5*TIMESCALE+0.5)
 
     allcfs.land(targetHeight=0.05, duration=2.0)
-    timeHelper.sleep(3.0)
+    timeHelper.sleep(2.5)
